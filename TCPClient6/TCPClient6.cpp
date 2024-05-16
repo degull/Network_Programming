f968b5a7871f9035de79b8ac7ca360adac8f51fb
DEGULL <99521386+degull@users.noreply.github.com>
2024-05-16 18:03:12 +0900

    TCP 서버-클라이언트
    TCP 서버-클라이언트

---
+++ TCPClient6/TCPClient6.cpp
@@ -0,0 +1,93 @@
+#include <stdio.h>
+//#include "Common.h"
+#include <winsock2.h> // Windows 소켓 프로그래밍을 위한 헤더 파일
+#include <ws2tcpip.h> // getaddrinfo 함수를 사용하기 위한 헤더 파일
+#pragma comment(lib, "ws2_32.lib")
+
+
+char* SERVERIP = (char*)"::1";
+#define SERVERPORT 9000
+#define BUFSIZE    512
+
+void err_quit(const char* msg) {
+	fprintf(stderr, "%s\n", msg);
+	exit(1);
+}
+
+void err_display(const char* msg) {
+	fprintf(stderr, "%s: %d\n", msg, WSAGetLastError());
+}
+
+int main(int argc, char* argv[])
+{
+	int retval;
+
+	// 명령행 인수가 있으면 IP 주소로 사용
+	if (argc > 1) SERVERIP = argv[1];
+
+	// 윈속 초기화
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		return 1;
+
+	// 소켓 생성
+	SOCKET sock = socket(AF_INET6, SOCK_STREAM, 0);
+	if (sock == INVALID_SOCKET) err_quit("socket()");
+
+	// connect()
+	struct sockaddr_in6 serveraddr;
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sin6_family = AF_INET6;
+	inet_pton(AF_INET6, SERVERIP, &serveraddr.sin6_addr);
+	serveraddr.sin6_port = htons(SERVERPORT);
+	retval = connect(sock, (struct sockaddr*)&serveraddr, sizeof(serveraddr));
+	if (retval == SOCKET_ERROR) err_quit("connect()");
+
+	// 데이터 통신에 사용할 변수
+	char buf[BUFSIZE + 1];
+	int len;
+
+	// 서버와 데이터 통신
+	while (1) {
+		// 데이터 입력
+		printf("\n[보낼 데이터] ");
+		if (fgets(buf, BUFSIZE + 1, stdin) == NULL)
+			break;
+
+		// '\n' 문자 제거
+		len = (int)strlen(buf);
+		if (buf[len - 1] == '\n')
+			buf[len - 1] = '\0';
+		if (strlen(buf) == 0)
+			break;
+
+		// 데이터 보내기
+		retval = send(sock, buf, (int)strlen(buf), 0);
+		if (retval == SOCKET_ERROR) {
+			err_display("send()");
+			break;
+		}
+		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
+
+		// 데이터 받기
+		retval = recv(sock, buf, retval, MSG_WAITALL);
+		if (retval == SOCKET_ERROR) {
+			err_display("recv()");
+			break;
+		}
+		else if (retval == 0)
+			break;
+
+		// 받은 데이터 출력
+		buf[retval] = '\0';
+		printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
+		printf("[받은 데이터] %s\n", buf);
+	}
+
+	// 소켓 닫기
+	closesocket(sock);
+
+	// 윈속 종료
+	WSACleanup();
+	return 0;
+}
